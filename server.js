0a34b4a2a0480529c81afe5eba9337031867fd91
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-08 17:01:19 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const { DB_HOST, PORT } = process.env;
+const { DB_HOST = '', PORT = 3000 } = process.env;
   
 mongoose.connect( DB_HOST )
   .then(() => {
     console.log("Database connection successful");
-    app.listen( PORT, () => console.log("Server running. Use our API on port: 3000") );
+    app.listen( PORT, () => console.log(`Server running. Use our API on port: ${PORT}`) );
   })
   .catch((error) => {
-    console.log(error.codeName); 
+    console.log(error.massage); 
     process.exit(1); 
   });
