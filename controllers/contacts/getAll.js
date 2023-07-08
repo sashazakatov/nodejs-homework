b099d071ffa7dc04304be54ded125666ecb96bf1
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-09 00:24:38 +0300

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,8 +1,21 @@
 const { Contact } = require('../../models');
 
 const getAll = async (req, res, next) => {
-    // console.log(req.user._id);
-    const result = await Contact.find({ owner: req.user._id });
+    const page = parseInt(req.query?.page) || 1;
+    const limit = parseInt(req.query?.limit) || 10;
+    const { favorite } = req.query;
+
+    const startIndex = (page - 1) * limit; 
+    const endIndex = page * limit;
+
+    const query = { owner: req.user._id };
+    if(favorite){
+        query.favorite = favorite
+    }
+
+    const result = await Contact.find( query )
+        .skip(startIndex)
+        .limit(endIndex);
     res.json(result);
 }
 module.exports = getAll;
