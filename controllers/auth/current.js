026bcc6c13f0886fdcc0d4b942cbf12e635c789c
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-08 23:23:03 +0300

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,4 +1,15 @@
-const current = async() => {
+const { User } = require('../../models');
+const { HttpError } = require('../../helpers');
 
+const current = async(req, res, next) => {
+    const user = await User.findById(req.user.id);
+    if(!user){
+        throw HttpError({ status: 401, message: "Not authorized" });
+    }
+
+    res.json({
+        email: user.email,
+        subscription: user.subscription,
+    });
 }
 module.exports = current;
