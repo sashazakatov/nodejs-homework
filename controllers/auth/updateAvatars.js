93c7138925c22f22d2bf1ca291feea0f6d666727
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-13 12:02:59 +0300

    update controllers

--- controllers/auth/updateAvatars.js
+++ controllers/auth/updateAvatars.js
@@ -1,11 +1,18 @@
+const jimp = require("jimp");
 const path = require('path');
 const fs = require('fs/promises');
-const { User } = require('../../models')
+const { User } = require('../../models');
+const { HttpError } = require('../../helpers');
 
 const avatarsDir = path.join(__dirname, '../../', 'public/avatars');
 
 
 const updateAvatars = async(req, res, next) => {
+    
+    if(!req.file) {
+        throw HttpError({ status: 401 });
+    }
+
     const { _id } = req.user;
     const { path: tempUpload, originalname } = req.file;
 
@@ -13,12 +20,14 @@ const updateAvatars = async(req, res, next) => {
     const resoltUpload = path.join(avatarsDir, fileName);
     const avatarURL = path.join('avatars', fileName);
 
+    const image = await jimp.read(tempUpload);
+    const newImage = await image.cover(250, 250);
+    newImage.write(tempUpload);
+
     await fs.rename(tempUpload, resoltUpload);
 
     await User.findByIdAndUpdate(_id, {avatarURL});
 
-    res.json({
-        avatarURL
-    });
+    res.json({ avatarURL });
 }
 module.exports = updateAvatars;
