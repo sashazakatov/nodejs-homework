d537370a90548d4b2f1f51fc64e2ffa5ae94b90d
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-16 10:12:01 +0300

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -3,19 +3,13 @@ const bcrypt = require('bcrypt');
 const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 
-const { usersSchemes } = require('../../schemes');
-
 const { SECRET_WORD } = process.env;
 
 const login = async(req, res, next) => {
-    const { error, value = '' } = usersSchemes.registerScheme.validate(req.body);
-    if(error){
-        throw HttpError({ status: 400, message: "Bad Request" });
-    }
 
-    const existingUser = await User.findOne({ email: value.email });
+    const existingUser = await User.findOne({ email: req.body });
     const password = existingUser?.password ?? '';
-    const match = await bcrypt.compare(value.password, password);
+    const match = await bcrypt.compare(req.body.password, password);
 
     if(!existingUser || !match){
         throw HttpError({ status: 401, message: "Email or password is wrong" })
